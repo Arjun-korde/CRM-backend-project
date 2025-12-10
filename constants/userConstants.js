c69a257797a31d7699cdbeeec7f38c3d948c7edd
Arjun korde <arjunkorde2004@gmail.com>
2025-12-10 22:15:37 +0530

    add user constants

---
+++ constants/userConstants.js
@@ -0,0 +1,13 @@
+module.exports = {
+    USER_TYPES: {
+        CUSTOMER: "CUSTOMER",
+        ENGINEER: "ENGINEER",
+        ADMIN: "ADMIN" 
+    },
+
+    USER_STATUS: {
+        PENDING: "PENDING",
+        APPROVED: "APPROVED",
+        BLOCKED: "BLOCKED"
+    }
+}
