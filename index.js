e0660a2bc85521640ed9cc89a5bd60229a6b7835
Arjun korde <arjunkorde2004@gmail.com>
2025-12-10 18:23:39 +0530

    add user export user model, setup admin creation

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
 require('dotenv').config();
+const User = require('./models/user.model');
+const bcrypt = require('bcrypt');
+
+// connect to mongoDB
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB connected successfully');
+
+        // default admin setup
+        const user = await User.findOne({ userId: "admin" });
+
+        if(!user){
+            console.log('Admin not present');
+            // let's create a new admin
+            const admin = await User.create({
+                name: "Arjun",
+                userId: 'admin',
+                email: "arjunkorde2004@gmail.com",
+                userType: "ADMIN",
+                password: bcrypt.hashSync('arjun',10)
+            });
+            console.log("admin created: ", admin);
+            
+        } else {
+            console.log('Admin already present');
+        }
+
+
+     } catch (err) {
+        console.log("Error while mongoDB connection: ", err);
+
+    }
+})();
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
--- models/user.model.js
+++ models/user.model.js
@@ -35,3 +35,6 @@ const userSchema = new mongoose.Schema({
 },
     { timestamps: true }
 );
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
