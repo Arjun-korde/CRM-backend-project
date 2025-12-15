e403d27cb3a9077a758b3baabc7dd94adf4799ac
Arjun korde <arjunkorde2004@gmail.com>
2025-12-15 23:24:37 +0530

    feat(user): add auth middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+const { verifyToken } = require("../utils/token.util");
+require('dotenv').config();
+
+const validateToken = (req, res, next) => {
+
+    const authHeader = req.get('authorization') || req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({ message: "MESSAGES.AUTH.TOKEN_MISSING" });
+    }
+
+    const token = authHeader.slice(7).trim();
+    console.log('token : ', token);
+    
+    if (!token) {
+        throw new Error('Missing access token');
+    }
+
+    const payload = verifyToken(token, process.env.JWT_SECRET);
+    if(!payload) {
+        throw new Error('Invalid token');
+    }
+
+    req.userId = payload.userId;
+
+    next();
+
+}
+
+module.exports = {
+    validateToken,
+
+}
